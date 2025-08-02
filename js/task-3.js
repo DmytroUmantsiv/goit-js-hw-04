eb5640cac02abebd6f6dee0a1bfa3bb611e5039d
Dmytro <dmutro15um10@gmail.com>
2025-08-02 15:43:19 +0200

    4 homework

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,24 @@
-function getElementWidth(content, padding, border) {
-  let numContent = parseFloat(content);
-  let numPadding = parseFloat(padding);
-  let numBorder = parseFloat(border);
-  let fullWidth = numContent + (numPadding + numBorder) * 2;
-  return fullWidth;
-}
-console.log(getElementWidth('50px', '8px', '4px'));
-console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('200px', '0px', '0px'));
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+
+  changeUsername(newName) {
+    profile.username = newName;
+  },
+
+  updatePlayTime(hours) {
+    profile.playTime += hours;
+  },
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  }
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
